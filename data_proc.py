62103cd3a8eb16503f086d73267693091d4839d6
Akash tesla <68317027+akashktesla@users.noreply.github.com>
2022-05-03 23:26:45 +0530

    :)

--- data_proc.py
+++ data_proc.py
@@ -1,21 +1,28 @@
-import numpy as np
-import cv2
+import cv2 as cv
+import mediapipe as mp
+import time
+import math
+
+vcap = cv.VideoCapture('http://192.168.43.41:5000/video_feed')
+mphands=mp.solutions.hands
+hands=mphands.Hands()
+mpDraw= mp.solutions.drawing_utils
 
 
-vcap = cv2.VideoCapture('http://192.168.43.41:5000/video_feed')
+while True:
+    success,img = vcap.read()
 
-while(True):
+    imgRGB=cv.cvtColor(img,cv.COLOR_BGR2RGB)
+    results= hands.process(imgRGB)
+    if results.multi_hand_landmarks:
+        for handlmks in results.multi_hand_landmarks:
+            for id,lm in enumerate(handlmks.landmark):
+                # print(id,lm)
+                h,w,c = img.shape
+                cx,cy=int(lm.x*w),int(lm.y*h)
+                print(id,cx,cy)
+            mpDraw.draw_landmarks(img, handlmks, mphands.HAND_CONNECTIONS)
 
-    ret, frame = vcap.read()
-
-    if frame is not None:
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(22) & 0xFF == ord('q'):
-            break
-    else:
-        print("Frame is None")
-        break
-
-vcap.release()
-cv2.destroyAllWindows()
-print("Video stop")
+    cv.imshow('image',img)
+    cv.waitKey(1)
+cv.destroyAllWindows()
